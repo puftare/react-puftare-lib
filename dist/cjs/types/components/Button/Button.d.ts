33852e7fc0ddf00f7f71ae6ff8447753957bfe31
Martin Potkonjak <56492445+puftare@users.noreply.github.com>
2023-02-01 16:19:17 +0100

    delete dist

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface ButtonProps {
-    name: string;
-}
-declare const Button: (props: ButtonProps) => JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface ButtonProps {
-    name: string;
-}
-declare const Button: (props: ButtonProps) => JSX.Element;
-
-export { Button };
