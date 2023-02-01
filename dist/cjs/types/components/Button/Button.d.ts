daa751a90b51479e4fa90aea568ce71b99c0a9f3
puftare <martin.potkonjak@hotmail.com>
2023-02-01 16:07:16 +0100

    added script for rollup bundling

---
+++ dist/cjs/types/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+interface ButtonProps {
+    name: string;
+}
+declare const Button: (props: ButtonProps) => JSX.Element;
+export default Button;
--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,17 @@ import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
-
 export default [
   {
     input: "./src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: "dist/cjs/index.js",
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: "dist/esm/index.js",
         format: "esm",
         sourcemap: true,
       },
